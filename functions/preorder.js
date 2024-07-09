1a805f4f9abc0bb8c241d1270be37a0f82620870
Harmony-fashion <harmonyfashion.us@gmail.com>
2024-07-09 23:49:23 +0530

    Update preorder.js

--- functions/preorder.js
+++ functions/preorder.js
@@ -1,8 +1,10 @@
 const { google } = require('googleapis');
 const sheets = google.sheets('v4');
 const { GoogleAuth } = require('google-auth-library');
+const path = require('path')
+
 const auth = new GoogleAuth({
-  keyFile: 'path/to/your/service-account-file.json',
+  keyFile: path.join(__dirname, 'path/to/your/service-account-file.json'),
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
@@ -10,7 +12,7 @@ exports.handler = async (event, context) => {
   const { email, imageId } = JSON.parse(event.body);
   const authClient = await auth.getClient();
   const spreadsheetId = '15Gs1QYUTiHQDJP2f0ztKOd7vw-kA0tcG00cE1rQY-yI';
-  const range = 'Sheet1!A1:D1'; // Adjust according to your sheet structure
+  const range = 'Sheet1!A1:D1'; 
 
   const request = {
     spreadsheetId,
